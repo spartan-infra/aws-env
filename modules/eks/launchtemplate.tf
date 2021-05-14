92a2924539f95ee09c5dd6bbf056d8f356d740f5
Damian Bruera <brueradamian@gmail.com>
2021-05-14 11:19:32 -0300

    [AWS-EKS] Adding EKS Cluster
    We are adding the modules and resources
    needed by AWS/Terraform to create a basic
    EKS cluster.
    
    Signed-off-by: Damian Bruera <brueradamian@gmail.com>

---
+++ modules/eks/launchtemplate.tf
@@ -0,0 +1,44 @@
+resource "aws_launch_template" "cluster_launch_template" {
+  name_prefix            = var.k8s_project_name_prefix
+  description            = format("Launch-Template created for %s project", var.k8s_project_name)
+  update_default_version = true
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size           = lookup(local.disk_parameters, "size")
+      volume_type           = lookup(local.disk_parameters, "type")
+      delete_on_termination = lookup(local.disk_parameters, "termination_delete")
+    }
+  }
+
+  monitoring {
+    enabled = lookup(local.instance_parameters, "monitoring")
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    delete_on_termination       = true
+    security_groups             = [module.eks.worker_security_group_id]
+  }
+
+  # Supplying custom tags to EKS instances is another use-case for LaunchTemplates
+  tag_specifications {
+    resource_type = "instance"
+    tags          = local.instance_tags
+  }
+
+  # Supplying custom tags to EKS instances root volumes is another use-case for LaunchTemplates. (doesnt add tags to dynamically provisioned volumes via PVC tho)
+  tag_specifications {
+    resource_type = "volume"
+    tags          = local.volume_tags
+  }
+
+  # Tag the LT itself
+  tags = local.launch_template_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
